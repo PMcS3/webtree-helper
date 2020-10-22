9fe20c96c54466b509b111aa8c6d8a76b03a22d6
Will Smith <wismith@davidson.edu>
2020-10-22 15:16:09 -0400

    messing around with DiagrammeR

--- app.R
+++ app.R
@@ -28,7 +28,19 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           # plotOutput("distPlot")
+            grViz("
+                digraph boxes_and_circles {
+                    node [shape = circle]
+                    A; B; C; D; E; F
+                    
+                    node [shape = box]
+                    1; 2; 3; 4; 5; 6; 7; 8
+                    
+                    A->1; B->2; B->3; B->4; C->A;
+                }
+            ")
+           
         )
     )
 )
--- ui.R
+++ ui.R
@@ -1,5 +1,6 @@
 library(shiny)
 library(sortable)
+library(DiagrammeR)
 
 ui <- fluidPage(
   
@@ -18,7 +19,18 @@ ui <- fluidPage(
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("distPlot")
+      # plotOutput("distPlot")
+      grViz("
+                digraph boxes_and_circles {
+                    node [shape = circle]
+                    A; B; C; D; E; F
+                    
+                    node [shape = box]
+                    1; 2; 3; 4; 5; 6; 7; 8
+                    
+                    A->1; B->2; B->3; B->4; C->A;
+                }
+            ")
     )
   )
 )
